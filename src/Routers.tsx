ff7b42a0a90bdbd9c3ebd3c08a96c1884b608a97
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-08-15 13:20:47 +0800

    update route stack and create home Screen

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,18 +1,27 @@
 
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { createStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './screen/SplashScreen';
+import HomeScreen from './screen/HomeScreen';
+
+
+export type RoutersParamList = {
+    SplashScreen: undefined,
+    HomeScreen: undefined
+}
 
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator<RoutersParamList>();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
