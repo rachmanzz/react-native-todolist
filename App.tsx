39365b6ee2adfa0cca74f2f8ff67b57c9b641479
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-08-15 10:40:37 +0800

    install & test react-navigation

---
+++ App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Routers from './src/Routers';
+
+export default function () {
+  return <Routers />
+}
